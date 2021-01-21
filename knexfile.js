15f6c5c00025cd1be0365ed56e03880b14757edb
Matthew Martin <zhintateo19924559@gmail.com>
2021-01-21 11:22:04 -0600

    Knex init, migrate working on

---
+++ knexfile.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: process.env.DB_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+  },
+
+  staging: {
+    client: 'pg',
+    connection: process.env.DB_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DB_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+  }
+
+};
