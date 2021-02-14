666531963b610948de6224b826798bcdfb20353a
Matthew Martin <zhintateo19924559@gmail.com>
2021-02-13 19:18:25 -0600

    Added the session libary and setup for sessions

--- api/server.js
+++ api/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const session = require('express-session');
 const helmet = require('helmet');
+const knexSessionStore = require('connect-session-knex');
 
 const cors = require('cors');
 
@@ -9,9 +11,30 @@ const ProjectRouter = require('./project/project-router');
 
 const server = express();
 
-server.use(cors())
-server.use(helmet())
-server.use(express.json())
+const sessionConfig = {
+	name: 'mjmsession',
+	secret: '7bfece61a4dbbc7b427355fd1499a851',
+	cookie: {
+		maxAge: 1000 * 60 * 60,
+		secure: false,
+		httpOnly: true
+	},
+	resave: false,
+	saveUninitialized: false,
+
+	store: new knexSessionStore({
+		knex: require("../data/dbConfig.js"),
+		tablename: "sessions",
+		sidfieldname: "sid",
+		createtable: true,
+		clearInterval: 1000 * 60 * 60
+	})
+}
+
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
+server.use(session(sessionConfig));
 
 server.use('/api/bugtracker', BugTrackerRouter);
 server.use('/api/blog', BlogRouter);
@@ -30,7 +53,7 @@ server.use((err, req, res, next) => {
 	res.status(err.statusCode).json({
 		mes: err.message,
 		stack: err.stack
-	})
-})
+	});
+});
 
 module.exports = server;
